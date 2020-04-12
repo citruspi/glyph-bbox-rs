d3d18cc8a823c41eda15c0f33eccb43f67e54228
Mihir Singh <git.service@mihirsingh.com>
2020-04-12 20:07:08 +0000

    Implemented bounding box calculation

--- assets/web/main.js
+++ assets/web/main.js
@@ -22,10 +22,55 @@ function loadConfig() {
     }
 }
 
+function generateDataSet(config) {
+    let dataset = {
+        config,
+        data: {}
+    };
+
+    let paper = Raphael(0, 0, 0, 0);
+
+    config.font.faces.forEach(function(face) {
+        dataset.data[face] = {};
+
+        config.font.sizes.forEach(function(size) {
+            dataset.data[face][size] = {
+                'boxes': draw(paper, face, size, config.char.offset, config.char.range)
+            };
+        });
+    });
+
+    paper.remove();
+
+    return dataset;
+}
+
+function draw(paper, fontFace, fontSize, charOffset, charRange) {
+    let boxes = [];
+
+    for (let x=charOffset; x<=charRange; x++) {
+        let str = paper.text(0, 0, String.fromCharCode(x));
+
+        str.attr('font-family', fontFace);
+        str.attr('font-size', fontSize);
+
+        let boundingBox = str.getBBox();
+
+        str.remove();
+
+        boxes.push([boundingBox.width, boundingBox.height]);
+    }
+
+    return boxes;
+
+}
+
 window.onload = function() {
     let config = loadConfig();
 
     if (config['error'] !== undefined) { console.log(config); return }
 
-    console.log(config);
+    let dataset = generateDataSet(config);
+
+    console.log(dataset);
 };
