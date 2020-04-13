93efca95fa48b9caf6d9e366b341b44e53039442
Mihir Singh <git.service@mihirsingh.com>
2020-04-13 03:25:45 +0000

    Implemented support for writing a DataSet to a file

--- src/dataset/mod.rs
+++ src/dataset/mod.rs
@@ -2,6 +2,8 @@ extern crate serde;
 extern crate serde_json;
 
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::{Error, Write};
 
 use serde::{Deserialize, Serialize};
 use serde_json::Serializer as JsonSerializer;
@@ -42,4 +44,29 @@ pub struct DataSet {
 pub struct DataSetConfig {
     pub font: FontConfig,
     pub char: CharConfig,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct WriteOptions {
+    pub filename: String,
+    pub format: Format,
+}
+
+#[derive(Debug, Deserialize)]
+pub enum Format {
+    JSON,
+}
+
+impl DataSet {
+    pub fn write(&self, opts: WriteOptions) -> Result<(), Error> {
+        let mut buf = Vec::new();
+
+        match opts.format {
+            Format::JSON => self.serialize(&mut JsonSerializer::new(&mut buf)).unwrap(),
+        }
+
+        let mut file = File::create(opts.filename).unwrap();
+
+        file.write_all(buf.as_ref())
+    }
 }
